5de6fe30dfc623e9f182f7ba1fb3017108bd320f
bannzai <kingkong999yhirose@gmail.com>
2022-01-12 20:19:26 +0900

    :sparkles: skip private type

--- nodecheck.go
+++ nodecheck.go
@@ -64,6 +64,10 @@ func run(excludes string) func(pass *gqlanalysis.Pass) (interface{}, error) {
 		}
 
 		for _, t := range needToNodeTypes {
+			// Skip private type. e.g) __Directive, __Enum ...
+			if strings.HasPrefix("__") {
+				break
+			}
 			pass.Reportf(t.Position, "%+v should conform to Node", t.Name)
 		}
 
